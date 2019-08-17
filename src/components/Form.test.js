76838832f264bea2bed4f970623d85fca1b9c17a
Brianna DelValle <briannadelvalle@gmail.com>
2019-08-17 17:34:54 -0600

    Wrote Form tests

--- src/components/Form.test.js
+++ src/components/Form.test.js
@@ -1,17 +1,63 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 import Form from './Form';
+import './Form.css';
 
-it('should match the snapshot', () => {
-  const wrapper = shallow(<Form />)
+describe('Form', ()=> {
 
-  expect(wrapper).toMatchSnapshot();
+
+
+  
+  it('should match the snapshot', () => {
+    const wrapper = shallow(<Form />)
+  
+    expect(wrapper).toMatchSnapshot();
+  })
+  
+  //How to do this without spies?
+  
+  it('should fire handleChange to update state when name is edited', () => {
+    const wrapper = shallow(<Form />);
+     
+    let nameInput = wrapper.find('input[name="name"]');
+    
+    nameInput.simulate('change', { target: { name: 'name', value: 'Brianna'}});
+  
+    expect(wrapper.instance().state.name).toEqual('Brianna');
+  })
+  
+  it('should fire handleChange to update state when quote is edited', () => {
+    const wrapper = shallow(<Form />);
+     
+    let nameInput = wrapper.find('input[name="quote"]');
+    
+    nameInput.simulate('change', { target: { name: 'quote', value: 'Yolo'}});
+  
+    expect(wrapper.instance().state.quote).toEqual('Yolo');
+  })
+  
+  it('should fire handleChange to update state when superlative is edited', () => {
+    const wrapper = shallow(<Form />);
+     
+    let nameInput = wrapper.find('input[name="superlative"]');
+    
+    nameInput.simulate('change', { target: { name: 'superlative', value: 'Most likely to tell David to calm down'}});
+  
+    expect(wrapper.instance().state.superlative).toEqual('Most likely to tell David to calm down');
+  
+  })
+
+  it('should fire handleAddNewStudent when button is clicked', () => {
+    const wrapper = shallow(<Form />)
+    const mockHandleAddNewStudent = jest.fn();
+    
+
+    wrapper.find('button').simulate('click')
+
+    
+  })
 })
 
-//Test each input contenteditable
-
-//Test each input onChange firing handleChange
 
 //Test button click firing handleAddNewStudent
 
-//Test handleChange
