6014e0532812256de943647c5ad486e171757bf0
Brianna DelValle <briannadelvalle@gmail.com>
2019-08-19 01:12:42 -0600

    Refactor Person editing function flow

--- src/components/Person/Person.js
+++ src/components/Person/Person.js
@@ -2,23 +2,24 @@ import React, { Component } from 'react';
 import './Person.css';
 
 class Person extends Component {
-  constructor({ person, cohortType, deletePerson, updatePeople}) {
-    super()
+  constructor(props) {
+    super(props)
     this.state = {
-      name: person.name,
-      id: person.id,
-      quote: person.quote,
-      superlative: person.superlative,
-      photo: person.photo,
-      cohortType: cohortType, 
-      deletePerson: deletePerson,
-      updatePeople: updatePeople,
-      handleCompletedChange: () => {this.state.updatePeople(this.state)}
-    }
+      name: props.person.name,
+      id: props.person.id,
+      quote: props.person.quote,
+      superlative: props.person.superlative,
+      photo: props.person.photo,
+      cohortType: props.cohortType, 
+      deletePerson: props.deletePerson,
+      updatePeople: props.updatePeople
+    };
   }
 
-handleEdits(e, partToUpdate) {
-  this.setState({[e.target.id]: e.target.innerText})
+
+handleCompletedChange = (e) => {
+  this.setState({[e.target.id]: e.target.innerText});
+  this.state.updatePeople(this.state);
 }
   
 render() {
@@ -32,8 +33,7 @@ render() {
         id="name" 
         contentEditable="true"
         suppressContentEditableWarning={true}
-        onKeyUp={(e) => this.handleEdits(e, 'name')}
-        onBlur={this.state.handleCompletedChange}>
+        onBlur={this.handleCompletedChange}>
         {this.state.name}
       </h3>
       <div className="quote">
@@ -42,9 +42,8 @@ render() {
           id="quote" 
           contentEditable="true"
           suppressContentEditableWarning={true}
-          onKeyUp={e => this.handleEdits(e, 'quote')}
-          onBlur={this.state.handleCompletedChange}>
-          "{this.state.quote}"
+          onBlur={this.handleCompletedChange}>
+          {this.state.quote}
         </p>
       </div>
       <div className="superlative">
@@ -53,8 +52,7 @@ render() {
           id="superlative" 
           contentEditable="true"
           suppressContentEditableWarning={true}
-          onKeyUp={e => this.handleEdits(e, 'superlative')}
-          onBlur={this.state.handleCompletedChange}>
+          onBlur={this.handleCompletedChange}>
           {this.state.superlative}
         </h4>
       </div>
--- src/components/Person/Person.test.js
+++ src/components/Person/Person.test.js
@@ -9,6 +9,7 @@ let person = {id: 8, name: 'Robbie', quote: 'Cool.', superlative: 'Most Likely t
 describe('Person', () => {
   beforeEach(() => {
     wrapper = shallow(<Person key={person.name} cohortType="staff" updatePople={jest.fn()} deletePerson={jest.fn()} person={person} />)
+
     instance = wrapper.instance();
   })
   it('should match the snapshot with all correct data passed in', () => {
@@ -16,38 +17,10 @@ describe('Person', () => {
   })
 
   describe('Editing Contents', () => {
-  
-    it('should update state when name is edited', () => {
-      const mockEvent = { target: {
-        id: 'name', 
-        innerText: 'Robert J.'
-      }}
-      wrapper.find('#name').simulate('keyUp', mockEvent);
-      expect(instance.state.name).toEqual('Robert J.')
-    });
-
-    it('should update state when quote is edited', () => {
-      const mockEvent = { target: {
-        id: 'quote', 
-        innerText: 'Yolo?'
-      }}
-      wrapper.find('#quote').simulate('keyUp', mockEvent);
-      expect(instance.state.quote).toEqual('Yolo?')
-    });
-
-    it('should update state when superlative is edited', () => {
-      const mockEvent = { target: {
-        id: 'superlative', 
-        innerText: 'Most Likely To Wear Birkenstocks'
-      }}
-
-      wrapper.find('#superlative').simulate('keyUp', mockEvent);
-      expect(instance.state.superlative).toEqual('Most Likely To Wear Birkenstocks')
-    })
-
     it('should fire handleCompletedChange when user clicks away after editing', () => {
       const mockHandleCompletedChange = jest.fn();
-      instance.setState({handleCompletedChange: mockHandleCompletedChange});
+      instance.handleCompletedChange = mockHandleCompletedChange;
+      instance.setState({updatePeople: jest.fn()})
       wrapper.find('#name').simulate('blur')
       expect(mockHandleCompletedChange).toHaveBeenCalled();
     })
